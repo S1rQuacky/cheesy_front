fad1e96d667669500ea5f925e5db05ff879147ac
Neide Urbina <neide.n.urbina@gmail.com>
2021-11-12 22:24:11 -0600

    all routes complete, CRUD complete

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,17 +4,61 @@ import Index from "../pages/Index";
 import Show from "../pages/Show";
 
 function Main(props){
+    const [cheese, setCheese] = useState(null);
+
+    const URL = "https://cheese-nu.herokuapp.com/cheese/";
+
+    const getCheese = async () => {
+        const response = await fetch(URL);
+        const data = await response.json()
+        setCheese(data);
+    };
+
+    const createCheese = async (queso) => {
+        await fetch(URL, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body:JSON.stringify(queso),
+        });
+        getCheese();
+    };
+
+    const updateCheese = async (queso, id) => {
+        await fetch(URL + id, {
+            method: "put",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(queso)
+        });
+        getCheese();
+    };
+
+    const deleteCheese = async (id) => {
+        await fetch(URL + id, {
+            method: "delete"
+        })
+        getCheese();
+    };
+
+    useEffect(() => getCheese(), []);
+
     return (
         <main>
             <Switch>
                 <Route exact path="/">
-                    <Index />
+                    <Index cheese={cheese} createCheese={createCheese} />
                 </Route>
                 <Route 
                     path="/cheese/:id"
                     render={(rp) => (
                         <Show 
                             {...rp}
+                            cheese={cheese}
+                            updateCheese={updateCheese}
+                            deleteCheese={deleteCheese}
                         />
                     )}
                 />
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,7 +1,63 @@
-
+import { useState } from "react";
 
 function Show(props){
-    return <h1>Show</h1>
+    const id = props.match.params.id
+    const cheese = props.cheese
+    const queso = cheese.find(p => p._id === id)
+
+    //state form
+    const [editForm, setEditForm] = useState(queso)
+
+    //handleChange
+    const handleChange = (event) => {
+        setEditForm({ ...editForm, [event.target.name]: event.target.value});
+    };
+
+    //handleSubmit
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        props.updateCheese(editForm, queso._id)
+        //redirect back to index
+        props.history.push("/")
+    };
+
+    const removeCheese = () => {
+        props.deleteCheese(queso._id)
+        props.history.push("/")
+    }
+
+    return (
+        <div className="queso">
+            <h1>{queso.name}</h1>
+            <h2>{queso.countryOfOrigin}</h2>
+            <img src={queso.image} alt={queso.name} />
+            <button onClick={removeCheese} id="delete">DELETE</button>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={editForm.name}
+                    name="name"
+                    placeholder="name"
+                    onChange={handleChange}
+                />
+                 <input
+                    type="text"
+                    value={editForm.countryOfOrigin}
+                    name="countryOfOrigin"
+                    placeholder="Country of Origin"
+                    onChange={handleChange}
+                />
+                 <input
+                    type="text"
+                    value={editForm.image}
+                    name="image"
+                    placeholder="image URL"
+                    onChange={handleChange}
+                />
+                <input type="submit" value="Update the Cheese" />
+            </form>
+        </div>
+    )
 }
 
 export default Show
